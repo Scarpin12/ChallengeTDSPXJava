caeb592baa8300058a7b0157f45b32afa6ad5ad3
unknown <joaopedroscarpin65@gmail.com>
2025-09-21 01:01:49 -0300

    Remove arquivos sensíveis do controle de versão

--- ChallengeJava/src/model/vo/Credenciais.java
+++
@@ -1,6 +0,0 @@
-package model.vo;
-public class Credenciais {
-    private String url = "jdbc:oracle:thin:@oracle.fiap.com.br:1521:orcl";
-    public static final String user = "rm565421";
-    public static final String password = "090407";
-}
